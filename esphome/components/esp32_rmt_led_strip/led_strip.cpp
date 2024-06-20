5f330dd0a5f5f5b9145aa11e6f45aa510c596002
dragonn <dragonn@op.pl>
2024-06-20 10:17:12 +0200

    wtf

--- esphome/components/esp32_rmt_led_strip/led_strip.cpp
+++ esphome/components/esp32_rmt_led_strip/led_strip.cpp
@@ -1,5 +1,5 @@
-#include <cinttypes>
 #include "led_strip.h"
+#include <cinttypes>
 
 #ifdef USE_ESP32
 
@@ -18,11 +18,13 @@ static const uint32_t RMT_CLK_FREQ = 80000000;
 static const uint8_t RMT_CLK_DIV = 2;
 
 void ESP32RMTLEDStripLightOutput::setup() {
+  return;
   ESP_LOGCONFIG(TAG, "Setting up ESP32 LED Strip...");
 
   size_t buffer_size = this->get_buffer_size_();
 
-  ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
+  ExternalRAMAllocator<uint8_t> allocator(
+      ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
   this->buf_ = allocator.allocate(buffer_size);
   if (this->buf_ == nullptr) {
     ESP_LOGE(TAG, "Cannot allocate LED buffer!");
@@ -37,8 +39,10 @@ void ESP32RMTLEDStripLightOutput::setup() {
     return;
   }
 
-  ExternalRAMAllocator<rmt_item32_t> rmt_allocator(ExternalRAMAllocator<rmt_item32_t>::ALLOW_FAILURE);
-  this->rmt_buf_ = rmt_allocator.allocate(buffer_size * 8);  // 8 bits per byte, 1 rmt_item32_t per bit
+  ExternalRAMAllocator<rmt_item32_t> rmt_allocator(
+      ExternalRAMAllocator<rmt_item32_t>::ALLOW_FAILURE);
+  this->rmt_buf_ = rmt_allocator.allocate(
+      buffer_size * 8); // 8 bits per byte, 1 rmt_item32_t per bit
 
   rmt_config_t config;
   memset(&config, 0, sizeof(config));
@@ -65,26 +69,29 @@ void ESP32RMTLEDStripLightOutput::setup() {
   }
 }
 
-void ESP32RMTLEDStripLightOutput::set_led_params(uint32_t bit0_high, uint32_t bit0_low, uint32_t bit1_high,
+void ESP32RMTLEDStripLightOutput::set_led_params(uint32_t bit0_high,
+                                                 uint32_t bit0_low,
+                                                 uint32_t bit1_high,
                                                  uint32_t bit1_low) {
-  float ratio = (float) RMT_CLK_FREQ / RMT_CLK_DIV / 1e09f;
+  float ratio = (float)RMT_CLK_FREQ / RMT_CLK_DIV / 1e09f;
 
   // 0-bit
-  this->bit0_.duration0 = (uint32_t) (ratio * bit0_high);
+  this->bit0_.duration0 = (uint32_t)(ratio * bit0_high);
   this->bit0_.level0 = 1;
-  this->bit0_.duration1 = (uint32_t) (ratio * bit0_low);
+  this->bit0_.duration1 = (uint32_t)(ratio * bit0_low);
   this->bit0_.level1 = 0;
   // 1-bit
-  this->bit1_.duration0 = (uint32_t) (ratio * bit1_high);
+  this->bit1_.duration0 = (uint32_t)(ratio * bit1_high);
   this->bit1_.level0 = 1;
-  this->bit1_.duration1 = (uint32_t) (ratio * bit1_low);
+  this->bit1_.duration1 = (uint32_t)(ratio * bit1_low);
   this->bit1_.level1 = 0;
 }
 
 void ESP32RMTLEDStripLightOutput::write_state(light::LightState *state) {
   // protect from refreshing too often
   uint32_t now = micros();
-  if (*this->max_refresh_rate_ != 0 && (now - this->last_refresh_) < *this->max_refresh_rate_) {
+  if (*this->max_refresh_rate_ != 0 &&
+      (now - this->last_refresh_) < *this->max_refresh_rate_) {
     // try again next loop iteration, so that this change won't get lost
     this->schedule_show();
     return;
@@ -126,39 +133,40 @@ void ESP32RMTLEDStripLightOutput::write_state(light::LightState *state) {
   this->status_clear_warning();
 }
 
-light::ESPColorView ESP32RMTLEDStripLightOutput::get_view_internal(int32_t index) const {
+light::ESPColorView
+ESP32RMTLEDStripLightOutput::get_view_internal(int32_t index) const {
   int32_t r = 0, g = 0, b = 0;
   switch (this->rgb_order_) {
-    case ORDER_RGB:
-      r = 0;
-      g = 1;
-      b = 2;
-      break;
-    case ORDER_RBG:
-      r = 0;
-      g = 2;
-      b = 1;
-      break;
-    case ORDER_GRB:
-      r = 1;
-      g = 0;
-      b = 2;
-      break;
-    case ORDER_GBR:
-      r = 2;
-      g = 0;
-      b = 1;
-      break;
-    case ORDER_BGR:
-      r = 2;
-      g = 1;
-      b = 0;
-      break;
-    case ORDER_BRG:
-      r = 1;
-      g = 2;
-      b = 0;
-      break;
+  case ORDER_RGB:
+    r = 0;
+    g = 1;
+    b = 2;
+    break;
+  case ORDER_RBG:
+    r = 0;
+    g = 2;
+    b = 1;
+    break;
+  case ORDER_GRB:
+    r = 1;
+    g = 0;
+    b = 2;
+    break;
+  case ORDER_GBR:
+    r = 2;
+    g = 0;
+    b = 1;
+    break;
+  case ORDER_BGR:
+    r = 2;
+    g = 1;
+    b = 0;
+    break;
+  case ORDER_BRG:
+    r = 1;
+    g = 2;
+    b = 0;
+    break;
   }
   uint8_t multiplier = this->is_rgbw_ || this->is_wrgb_ ? 4 : 3;
   uint8_t white = this->is_wrgb_ ? 0 : 3;
@@ -166,7 +174,9 @@ light::ESPColorView ESP32RMTLEDStripLightOutput::get_view_internal(int32_t index
   return {this->buf_ + (index * multiplier) + r + this->is_wrgb_,
           this->buf_ + (index * multiplier) + g + this->is_wrgb_,
           this->buf_ + (index * multiplier) + b + this->is_wrgb_,
-          this->is_rgbw_ || this->is_wrgb_ ? this->buf_ + (index * multiplier) + white : nullptr,
+          this->is_rgbw_ || this->is_wrgb_
+              ? this->buf_ + (index * multiplier) + white
+              : nullptr,
           &this->effect_data_[index],
           &this->correction_};
 }
@@ -177,36 +187,38 @@ void ESP32RMTLEDStripLightOutput::dump_config() {
   ESP_LOGCONFIG(TAG, "  Channel: %u", this->channel_);
   const char *rgb_order;
   switch (this->rgb_order_) {
-    case ORDER_RGB:
-      rgb_order = "RGB";
-      break;
-    case ORDER_RBG:
-      rgb_order = "RBG";
-      break;
-    case ORDER_GRB:
-      rgb_order = "GRB";
-      break;
-    case ORDER_GBR:
-      rgb_order = "GBR";
-      break;
-    case ORDER_BGR:
-      rgb_order = "BGR";
-      break;
-    case ORDER_BRG:
-      rgb_order = "BRG";
-      break;
-    default:
-      rgb_order = "UNKNOWN";
-      break;
+  case ORDER_RGB:
+    rgb_order = "RGB";
+    break;
+  case ORDER_RBG:
+    rgb_order = "RBG";
+    break;
+  case ORDER_GRB:
+    rgb_order = "GRB";
+    break;
+  case ORDER_GBR:
+    rgb_order = "GBR";
+    break;
+  case ORDER_BGR:
+    rgb_order = "BGR";
+    break;
+  case ORDER_BRG:
+    rgb_order = "BRG";
+    break;
+  default:
+    rgb_order = "UNKNOWN";
+    break;
   }
   ESP_LOGCONFIG(TAG, "  RGB Order: %s", rgb_order);
   ESP_LOGCONFIG(TAG, "  Max refresh rate: %" PRIu32, *this->max_refresh_rate_);
   ESP_LOGCONFIG(TAG, "  Number of LEDs: %u", this->num_leds_);
 }
 
-float ESP32RMTLEDStripLightOutput::get_setup_priority() const { return setup_priority::HARDWARE; }
+float ESP32RMTLEDStripLightOutput::get_setup_priority() const {
+  return setup_priority::HARDWARE;
+}
 
-}  // namespace esp32_rmt_led_strip
-}  // namespace esphome
+} // namespace esp32_rmt_led_strip
+} // namespace esphome
 
-#endif  // USE_ESP32
+#endif // USE_ESP32
--- esphome/components/remote_receiver/remote_receiver_esp32.cpp
+++ esphome/components/remote_receiver/remote_receiver_esp32.cpp
@@ -1,5 +1,5 @@
-#include "remote_receiver.h"
 #include "esphome/core/log.h"
+#include "remote_receiver.h"
 
 #ifdef USE_ESP32
 #include <driver/rmt.h>
@@ -10,6 +10,7 @@ namespace remote_receiver {
 static const char *const TAG = "remote_receiver.esp32";
 
 void RemoteReceiverComponent::setup() {
+  return;
   ESP_LOGCONFIG(TAG, "Setting up Remote Receiver...");
   this->pin_->setup();
   rmt_config_t rmt{};
@@ -20,11 +21,12 @@ void RemoteReceiverComponent::setup() {
     rmt.rx_config.filter_en = false;
   } else {
     rmt.rx_config.filter_en = true;
-    rmt.rx_config.filter_ticks_thresh = static_cast<uint8_t>(
-        std::min(this->from_microseconds_(this->filter_us_) * this->clock_divider_, (uint32_t) 255));
+    rmt.rx_config.filter_ticks_thresh = static_cast<uint8_t>(std::min(
+        this->from_microseconds_(this->filter_us_) * this->clock_divider_,
+        (uint32_t)255));
   }
-  rmt.rx_config.idle_threshold =
-      static_cast<uint16_t>(std::min(this->from_microseconds_(this->idle_us_), (uint32_t) 65535));
+  rmt.rx_config.idle_threshold = static_cast<uint16_t>(
+      std::min(this->from_microseconds_(this->idle_us_), (uint32_t)65535));
 
   esp_err_t error = rmt_config(&rmt);
   if (error != ESP_OK) {
@@ -38,7 +40,9 @@ void RemoteReceiverComponent::setup() {
   if (error != ESP_OK) {
     this->error_code_ = error;
     if (error == ESP_ERR_INVALID_STATE) {
-      this->error_string_ = str_sprintf("RMT channel %i is already in use by another component", this->channel_);
+      this->error_string_ =
+          str_sprintf("RMT channel %i is already in use by another component",
+                      this->channel_);
     } else {
       this->error_string_ = "in rmt_driver_install";
     }
@@ -64,25 +68,31 @@ void RemoteReceiverComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Remote Receiver:");
   LOG_PIN("  Pin: ", this->pin_);
   if (this->pin_->digital_read()) {
-    ESP_LOGW(TAG, "Remote Receiver Signal starts with a HIGH value. Usually this means you have to "
-                  "invert the signal using 'inverted: True' in the pin schema!");
+    ESP_LOGW(TAG,
+             "Remote Receiver Signal starts with a HIGH value. Usually this "
+             "means you have to "
+             "invert the signal using 'inverted: True' in the pin schema!");
   }
   ESP_LOGCONFIG(TAG, "  Channel: %d", this->channel_);
   ESP_LOGCONFIG(TAG, "  RMT memory blocks: %d", this->mem_block_num_);
   ESP_LOGCONFIG(TAG, "  Clock divider: %u", this->clock_divider_);
   ESP_LOGCONFIG(TAG, "  Tolerance: %" PRIu32 "%s", this->tolerance_,
-                (this->tolerance_mode_ == remote_base::TOLERANCE_MODE_TIME) ? " us" : "%");
-  ESP_LOGCONFIG(TAG, "  Filter out pulses shorter than: %" PRIu32 " us", this->filter_us_);
-  ESP_LOGCONFIG(TAG, "  Signal is done after %" PRIu32 " us of no changes", this->idle_us_);
+                (this->tolerance_mode_ == remote_base::TOLERANCE_MODE_TIME)
+                    ? " us"
+                    : "%");
+  ESP_LOGCONFIG(TAG, "  Filter out pulses shorter than: %" PRIu32 " us",
+                this->filter_us_);
+  ESP_LOGCONFIG(TAG, "  Signal is done after %" PRIu32 " us of no changes",
+                this->idle_us_);
   if (this->is_failed()) {
-    ESP_LOGE(TAG, "Configuring RMT driver failed: %s (%s)", esp_err_to_name(this->error_code_),
-             this->error_string_.c_str());
+    ESP_LOGE(TAG, "Configuring RMT driver failed: %s (%s)",
+             esp_err_to_name(this->error_code_), this->error_string_.c_str());
   }
 }
 
 void RemoteReceiverComponent::loop() {
   size_t len = 0;
-  auto *item = (rmt_item32_t *) xRingbufferReceive(this->ringbuf_, &len, 0);
+  auto *item = (rmt_item32_t *)xRingbufferReceive(this->ringbuf_, &len, 0);
   if (item != nullptr) {
     this->decode_rmt_(item, len);
     vRingbufferReturnItem(this->ringbuf_, item);
@@ -105,34 +115,37 @@ void RemoteReceiverComponent::decode_rmt_(rmt_item32_t *item, size_t len) {
   ESP_LOGVV(TAG, "START:");
   for (size_t i = 0; i < item_count; i++) {
     if (item[i].level0) {
-      ESP_LOGVV(TAG, "%zu A: ON %" PRIu32 "us (%u ticks)", i, this->to_microseconds_(item[i].duration0),
-                item[i].duration0);
+      ESP_LOGVV(TAG, "%zu A: ON %" PRIu32 "us (%u ticks)", i,
+                this->to_microseconds_(item[i].duration0), item[i].duration0);
     } else {
-      ESP_LOGVV(TAG, "%zu A: OFF %" PRIu32 "us (%u ticks)", i, this->to_microseconds_(item[i].duration0),
-                item[i].duration0);
+      ESP_LOGVV(TAG, "%zu A: OFF %" PRIu32 "us (%u ticks)", i,
+                this->to_microseconds_(item[i].duration0), item[i].duration0);
     }
     if (item[i].level1) {
-      ESP_LOGVV(TAG, "%zu B: ON %" PRIu32 "us (%u ticks)", i, this->to_microseconds_(item[i].duration1),
-                item[i].duration1);
+      ESP_LOGVV(TAG, "%zu B: ON %" PRIu32 "us (%u ticks)", i,
+                this->to_microseconds_(item[i].duration1), item[i].duration1);
     } else {
-      ESP_LOGVV(TAG, "%zu B: OFF %" PRIu32 "us (%u ticks)", i, this->to_microseconds_(item[i].duration1),
-                item[i].duration1);
+      ESP_LOGVV(TAG, "%zu B: OFF %" PRIu32 "us (%u ticks)", i,
+                this->to_microseconds_(item[i].duration1), item[i].duration1);
     }
   }
   ESP_LOGVV(TAG, "\n");
 
-  this->temp_.reserve(item_count * 2);  // each RMT item has 2 pulses
+  this->temp_.reserve(item_count * 2); // each RMT item has 2 pulses
   for (size_t i = 0; i < item_count; i++) {
     if (item[i].duration0 == 0u) {
       // Do nothing
-    } else if ((bool(item[i].level0) == prev_level) || (item[i].duration0 < filter_ticks)) {
+    } else if ((bool(item[i].level0) == prev_level) ||
+               (item[i].duration0 < filter_ticks)) {
       prev_length += item[i].duration0;
     } else {
       if (prev_length > 0) {
         if (prev_level) {
-          this->temp_.push_back(this->to_microseconds_(prev_length) * multiplier);
+          this->temp_.push_back(this->to_microseconds_(prev_length) *
+                                multiplier);
         } else {
-          this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) * multiplier);
+          this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) *
+                                multiplier);
         }
       }
       prev_level = bool(item[i].level0);
@@ -141,14 +154,17 @@ void RemoteReceiverComponent::decode_rmt_(rmt_item32_t *item, size_t len) {
 
     if (item[i].duration1 == 0u) {
       // Do nothing
-    } else if ((bool(item[i].level1) == prev_level) || (item[i].duration1 < filter_ticks)) {
+    } else if ((bool(item[i].level1) == prev_level) ||
+               (item[i].duration1 < filter_ticks)) {
       prev_length += item[i].duration1;
     } else {
       if (prev_length > 0) {
         if (prev_level) {
-          this->temp_.push_back(this->to_microseconds_(prev_length) * multiplier);
+          this->temp_.push_back(this->to_microseconds_(prev_length) *
+                                multiplier);
         } else {
-          this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) * multiplier);
+          this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) *
+                                multiplier);
         }
       }
       prev_level = bool(item[i].level1);
@@ -159,12 +175,13 @@ void RemoteReceiverComponent::decode_rmt_(rmt_item32_t *item, size_t len) {
     if (prev_level) {
       this->temp_.push_back(this->to_microseconds_(prev_length) * multiplier);
     } else {
-      this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) * multiplier);
+      this->temp_.push_back(-int32_t(this->to_microseconds_(prev_length)) *
+                            multiplier);
     }
   }
 }
 
-}  // namespace remote_receiver
-}  // namespace esphome
+} // namespace remote_receiver
+} // namespace esphome
 
 #endif
